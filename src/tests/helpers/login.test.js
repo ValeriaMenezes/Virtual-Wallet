5ad18872fe8b9596414c207d7032a217bc095842
Valéria <valeriamenezes022@gmail.com>
2022-10-03 16:09:22 -0300

    adiciona cobertura de testes requisito 5

--- src/tests/helpers/login.test.js
+++ src/tests/helpers/login.test.js
@@ -3,6 +3,8 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './renderWith';
 import App from '../../App';
+import mockData from './mockData';
+import Wallet from '../../pages/Wallet';
 
 describe('Testa a página de login', () => {
   it('Testa input de email, password e botão', () => {
@@ -21,5 +23,12 @@ describe('Testa a página de login', () => {
     userEvent.click(btn);
 
     expect(history.location.pathname).toBe('/carteira');
+  });
+  it('testa global fetch', () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+    renderWithRouterAndRedux(<Wallet />);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
   });
 });
