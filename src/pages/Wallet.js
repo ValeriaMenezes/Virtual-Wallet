d01f7f4f72122b2db64c826dbd3db1569e3048ae
Valéria <valeriamenezes022@gmail.com>
2022-10-06 09:59:41 -0300

    adiciona referências e corrige erros de lint

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,7 +5,7 @@ import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
-    // REFERÊNCIAS: Romulo Silva, Sérgio Ruza me ajudaram nos requisitos 1, 4, 7, 8
+    // REFERÊNCIAS: Romulo Silva, Sérgio Ruza me ajudaram nos requisitos 1, 4, 7, 8 e 9
     // Regex de validação de email: https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
     return (
       <div>
